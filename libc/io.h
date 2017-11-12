3540513bd67365bd740171df9c8ed7e5a44839ec
Ronak Chauhan <ronakchauhan97@gmail.com>
2017-11-12 12:30:40 +0530

    basic i/o functions

---
+++ libc/io.h
@@ -0,0 +1,32 @@
+#ifndef IO_H
+#define IO_H
+
+#include "types.h"
+#define VID_MEM 0xb8000
+
+void prints(char* s) {
+	uint16_t *mem_loc = (uint16_t*) VID_MEM;
+	static int mem_offset  = 0;
+	uint16_t out = 0x0700;
+
+	while(*s) {
+		out = out | (uint16_t)(*s);
+		*(mem_loc + mem_offset) = out;
+		mem_offset++;
+		out = 0x0700;
+		s++;
+	}
+}
+
+void clear() {
+	uint16_t *mem_loc = (uint16_t*) VID_MEM;
+	const uint16_t out = 0x0f00;
+	int mem_offset = 0;
+	
+	for(int16_t i = 0; i < 25*80; i++) {	
+		*(mem_loc + mem_offset) = out;
+		mem_offset++;
+	}
+}
+
+#endif
